f8a5361295b0284b66518936260eb5b58f26e3ea
Jonathan Partouche <jonathan@artesanosverdes.com>
2026-07-09 15:47:48 +0200

    config firebase

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.firebaseConfig = {
+  apiKey: "AIzaSyBIoyI1GHVePcvloM0O6m2aLYsZrhxDJrE",
+  authDomain: "artesanos-verdes-renovantis.firebaseapp.com",
+  projectId: "artesanos-verdes-renovantis",
+  storageBucket: "artesanos-verdes-renovantis.firebasestorage.app",
+  messagingSenderId: "610035047904",
+  appId: "1:610035047904:web:b1c0515d51929627079f85",
+  measurementId: "G-Z8Q0E6XQCL"
+};
